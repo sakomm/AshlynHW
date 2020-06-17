ecfc35660c239c36984d19c06489de17e004d7ca
Anurag kommuri <sakommuri@gmail.com>
2020-06-17 15:24:01 -0400

    sync

--- fileReader.py
+++ fileReader.py
@@ -1,19 +1,36 @@
-output = []
-file = open('test.txt')
-fileReader = file.read()
-fileReader = fileReader.strip().split()
-# at this point all the words in the file are split based open an empty space char 
-# aka spacebar and any unessecary space crap is gone
+Lines = []
 
+file = input('Enter the name of the file:')
+search = input('Enter the search term: ')
+#search1 = str(search)
 
-#print(fileReader) test 
-keyWords = ["poop"]
-# append values to compare
+#print('Alice' == 'alice')
 
-for words in fileReader:
-    for key in keyWords:
-        if words == key:
-            output.append(words)
+FileHandler = open(file)
+FileOut = open('Output.txt','w')
+Lines = FileHandler.readline()
+count = 1
+while Lines:
+    if search.lower() in Lines.lower():
+        FileOut.write(("{} : {} \n".format(count, Lines.strip())))
 
-print (output)
+    Lines = FileHandler.readline()
+    count += 1
+#Lines.strip()
+#print(Lines)
 
+'''
+
+    for i in f:
+        lines.append(i,rstrip('\n'))
+        
+for index, i in enumerate(lines):
+    if search.lower() in i.lower():
+        newlines.append(f'{index+1}: {i}')
+
+        print(index+1, i)
+
+with open('output.txt', 'w') as f:
+    for i in newlines:
+        f.write(i+'\n')
+'''
